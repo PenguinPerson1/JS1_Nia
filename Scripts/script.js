5cf9f18611ab486abb0d3ac5c4be82d3f4f608bb
Nia <nia.plossl@gmail.com>
2018-03-21 17:39:46 -0400

    .

--- Scripts/script.js
+++
@@ -1,6 +0,0 @@
-function MyFunction() {
-    document.getElementById("demo").innerHTML = "Thank you for liking penguins as much as I do!";
-    document.getElementById('outset').id = 'inset';
-    document.getElementById('regular').id = 'inverted';
-
-}
