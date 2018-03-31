6831ed68c5b2631b0d673504a7d35320dea37f1c
Nia <nia.plossl@gmail.com>
2018-03-30 20:33:42 -0400

    Had it start with only one button

--- Scripts/script.js
+++ Scripts/script.js
@@ -1,23 +1,31 @@
-function MyFunction() {
+$(document).ready (() => {
+
+  function MyFunction() {
     document.getElementById("demo").innerHTML = "Thank you for liking penguins as much as I do!";
     document.getElementById('outset').id = 'inset';
     document.getElementById('regular').id = 'inverted';
 
 }
+
 function Hide() {
   $("#HS").hide();
   $("#HSBH").hide();
   $("#HSBS").show();
 }
+
 function Show() {
   $("#HS").show();
   $("#HSBS").hide();
   $("#HSBH").show();
 }
 
+Show();
+
 $(".Hide").click( () => {
   Hide()
 });
 $(".Show").click( ()=> {
   Show()
-})
+});
+
+});
